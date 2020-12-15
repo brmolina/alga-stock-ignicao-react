440af86614426b4632244eb8220f1ff7066ad599
Bruno Molina <brmolina@gmail.com>
2020-12-15 19:49:08 -0300

    cicho de vida dos componentes

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
 class ClassComponent extends React.Component<{name: string}> {
+    constructor(props: any) {
+        super(props)
+        console.log('Constructor reached');
+        
+    }
+
     state = {
         name: 'Mundo !!!!!!'
     }
 
+    componentDidMount(){
+        console.log('did mount reached');
+        
+    }
+
+    componentDidUpdate(){
+        console.log('did update reached');
+        
+    }
+
     render() {
+        console.log('render reached');
+        
         return <div>
             <p>name: {this.state.name}</p>
             <button onClick={()=>{
