ff413d8d82db950ac88e78cfed53a0d93b5f1143
Bruno Molina <brmolina@gmail.com>
2020-12-15 10:59:45 -0300

    teste

---
+++ src/components/App/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import TestComponent from '../TestComponent'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TestComponent />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TestComponent.css'
+
+const TestComponent = () => (
+<div className="TestComponent">
+    Test Component
+</div>
+)
+
+export default TestComponent
