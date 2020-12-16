3112c68d4f33bf9c383f94d80cd5154bfeb7c320
Bruno Molina <brmolina@gmail.com>
2020-12-16 16:37:57 -0300

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Button from '../Button';
 import Header from '../Header';
 import './App.css';
+
+function TestComponent(){
+  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon"/>
+}
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock"/>
+
+      <div className="container">
+        <Button
+        onClic={() => window.alert('Alert personalizado')}
+        appendIcon={<TestComponent/>}
+        >
+          Potato
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { JsxElement } from 'typescript';
+import './Button.css'
+
+declare interface ButtonProps {
+    content?: string
+    onClic: () => void
+    appendIcon: JSX.Element
+}
+
+const Button: React.FC <ButtonProps> = (props) => {
+    return <button 
+        className="AppButton"
+        onClick = {props.onClic}
+        >
+        {props.children || 'Nameless button'}
+        {props.appendIcon}
+    </button>
+}
+
+export default Button;
