e318f45901c58672b049fc7279222079b0397bd2
Bruno Molina <brmolina@gmail.com>
2020-12-16 12:55:01 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import './App.css';
-import TestComponent from '../TestComponent'
-// import ClassComponent from '../ClassComponent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <TestComponent name="Bruno"/>
-        {/* <ClassComponent name="Mundo"/> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
