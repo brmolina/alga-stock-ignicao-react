ffe125ef6a6b2e3aa35f2a98a3b8ff07120693ee
Bruno Molina <brmolina@gmail.com>
2020-12-21 16:53:34 -0300

    clear app + create table components files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,37 +1,17 @@
-import React, { useState } from 'react';
-import Button from '../../shared/Button';
+import React from 'react';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
 import Header from '../Header';
 import './App.css';
 
-function TestComponent(){
-  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon"/>
-}
-
 function App() {
-
-  const [street, setStreet] = useState('')
 
   return (
     <div className="App">
       <Header title="AlgaStock"/>
 
         <Container>
-        {<ul>
-          {
-          ['Daniel', 'Willain', 'Thiago'].map((name, index) => 
-          {
-            return <li key={index}>
-              {name}
-            </li>
-          })
-        }
-        </ul>
-        }
-        </Container>
-        
       
+        </Container>
     </div>
   );
 }
---
+++ src/shared/Table/Table.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Table.scss'
+
+const Table: React.FC = ()=>{
+    return <table>
+            
+    </table>
+}
+
+export default Table
