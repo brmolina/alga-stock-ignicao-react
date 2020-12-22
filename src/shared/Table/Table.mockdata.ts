c547902921a790b7d9d9e953d9c8576746e27fd8
Bruno Molina <brmolina@gmail.com>
2020-12-22 16:36:58 -0300

    table mock data

---
+++ src/shared/Table/Table.mockdata.ts
@@ -0,0 +1,31 @@
+import { table } from "console"
+
+export interface Product {
+    id: number
+    name: string
+    price: number
+    stock: number
+}
+
+const Products: Product[] = [
+    {
+        id: 1,
+        name: 'Cookie',
+        price: 1.25,
+        stock: 12,
+    },
+    {
+        id: 2,
+        name: 'Potato',
+        price: 1.00,
+        stock: 15,
+    },
+    {
+        id: 3,
+        name: 'Milk',
+        price: 0.99,
+        stock: 20,
+    }
+]
+
+export default Products
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import './Table.scss'
+import Products from './Table.mockdata'
+
+const headers = [
+    {key:'name', value:'Product'},
+    {key:'price', value:'Price'},
+    {key:'stock', value:'Available Stock'},
+]
+
 
 const Table: React.FC = ()=>{
     return <table className="AppTable">
         <thead>
           <tr>
-          <th>Product</th>
-                <th>Price</th>
-                <th className="right">Stock</th>
+          {
+              headers.map(header => <th key={header.key}>{header.value}</th>)
+          }
           </tr>
         </thead>
 
         <tbody>
-            <tr>
-                <td>Cookie</td>
-                <td>1,25</td>
-                <td className="right">10</td>
-            </tr>
-            <tr>
-                <td>Potato</td>
-                <td>0,50</td>
-                <td className="right">8</td>
-            </tr>
+            {
+                 Products.map(product => <tr>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td className="right">{product.stock}</td>
+                </tr>)
+            }
         </tbody>
     </table>
 }
